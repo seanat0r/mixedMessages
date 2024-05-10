1f01db072121d0195d538a0a2b57b65d9c550e08
seanat0r <seanatorhd@gmail.com>
2024-05-10 12:50:13 +0200

    added random animal generator with 10 animals

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const randomAnimalGenerator = () => {
+    const animal = ['fish', 'dog', 'cat', 'bear', 'duck', 'lion', 'worm', 'bird', 'tiger', 'penguin'];
+    return animal[Math.floor(Math.random() * animal.length)];
+}
+
+let randomAnimal = randomAnimalGenerator();
+let randomFood;
+let randomPlace;
+
+
+
+console.log(randomAnimal);
