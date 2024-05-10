3206754a484c773c193067aa77e9204976acd14e
seanat0r <seanatorhd@gmail.com>
2024-05-10 20:12:44 +0200

    change the 3 let to a object. randomAnimalGenerator need to be updatet

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,11 +1,17 @@
 const randomAnimalGenerator = () => {
     const animal = ['fish', 'dog', 'cat', 'bear', 'duck', 'lion', 'worm', 'bird', 'tiger', 'penguin'];
     return animal[Math.floor(Math.random() * animal.length)];
-}
+};
 
-let randomAnimal = randomAnimalGenerator();
-let randomFood;
-let randomPlace;
+const randomFoodGenerator = () => {
+
+};
+
+let jokePart = {
+    animalList: ['fish', 'dog', 'cat', 'bear', 'duck', 'lion', 'worm', 'bird', 'tiger', 'penguin'],
+    foodList: ['chips'],
+    placeList: ['town']
+};
 
 
 
