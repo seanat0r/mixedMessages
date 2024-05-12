44d2dd91862ae1950af4ab3a622b82b560d132eb
seanat0r <seanatorhd@gmail.com>
2024-05-12 23:27:40 +0200

    Added object dogJoke. Added in dogJoke 3 arrays and 1 function.

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,4 +1,4 @@
-let jokePart = {
+let animalJoke = {
     animalList: ['fish', 'dog', 'cat', 'bear', 'duck', 'lion', 'worm', 'bird', 'tiger', 'penguin'],
     foodList: ['pizza', 'burger', 'pasta', 'salad', 'sushi', 'steak', 'chicken curry', 'fish and chips', 'tacos', 'sandwich'],
     placeList: ['forest', 'city', 'mountains', 'beach', 'desert', 'countryside', 'lake', 'valley', 'glacier', 'plains'],
@@ -43,5 +43,44 @@ let jokePart = {
 };
 
 //add here dogJoke
+let dogJoke = {
+    firstDog: ["Buddy", "Max", "Bailey", "Charlie", "Lucy", "Daisy", "Rocky", "Molly", "Sadie", "Cooper"],
+    secondDog: ["Luna", "Bear", "Bella", "Duke", "Rosie", "Oliver", "Lola", "Jack", "Sophie", "Toby"],
+    enviroment: ["Dog parks", "Beaches", "Hiking trails", "Pet stores", "Veterinary clinics", "Dog-friendly cafes", "City streets", "Dog daycare centers", "Dog shows", "Family homes"],
 
-jokePart.sayTheJoke();
+    generateRandomFirstDog () {
+        const holdFirstDog = [];
+        const randomValue = [];
+        let finishValueFirstDog;
+
+        for (i = 0; i < 5; i++) {
+            holdFirstDog.push(this.firstDog[Math.floor(Math.random() * this.firstDog.length)]);
+            randomValue.push(Math.floor(Math.random() * 20) + 1);
+        }
+        const randomSwitchCase = randomValue.reduce((a, b) => a * b) //multyplay all elemnts in the array
+
+        //checks which element of the array from holdFirstDog it should use, based of the product of randomValue.
+        if (randomSwitchCase > 0 && randomSwitchCase < 64000) {
+            return holdFirstDog[0];
+
+        } else if (randomSwitchCase > 64000 && randomSwitchCase < 128000) {
+            return holdFirstDog[1];
+
+        } else if (randomSwitchCase > 128000 && randomSwitchCase < 192000) {
+            return holdFirstDog[2];
+
+        } else if (randomSwitchCase > 192000 && randomSwitchCase < 256000) {
+            return holdFirstDog[3];
+
+        } else if (randomSwitchCase > 256000 && randomSwitchCase < 320000) {
+            return holdFirstDog[5];
+
+        } else {
+            return console.log(`The numbers are incorect! The Value of randomSwitchCase: ${randomSwitchCase} and the value of the array from randomValue is: ${randomValue}.`);
+        }
+    },
+}
+
+
+console.log(dogJoke.generateRandomFirstDog());
+animalJoke.sayTheJoke();
